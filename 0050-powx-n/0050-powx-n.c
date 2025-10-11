9d8dc62009c6058ef4447f4a62c56b62b5230805
Rahul Madhusudan Shettigar <rahulms2384@gmail.com>
2025-10-11 08:48:58 +0530

    Time: 0 ms (100%), Space: 7.9 MB (96.98%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.c
@@ -0,0 +1,13 @@
+double myPow(double x, long long int n) {
+    if(n==0) return 1;
+    if(n<0){
+        n= -n;
+        x = 1/x;
+    }
+    if(n%2==0){
+        return myPow(x*x,n/2);
+    }
+    else{
+        return x*myPow(x,n-1);
+    }
+}
