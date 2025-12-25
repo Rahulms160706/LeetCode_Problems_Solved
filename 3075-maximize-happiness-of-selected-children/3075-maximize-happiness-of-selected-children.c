9515923f693453feccaba49c03ff320ed7a34efd
Rahul Madhusudan Shettigar <rahulms2384@gmail.com>
2025-12-25 07:12:48 +0530

    Time: 134 ms (100%), Space: 21.6 MB (100%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.c
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.c
@@ -1,3 +1,4 @@
+/* too much time
 int cmp(const void *a, const void *b){
     return (*(int*)b - *(int*)a);
 }
@@ -10,6 +11,60 @@ long long maximumHappinessSum(int* happiness, int happinessSize, int k) {
     while(k){
         long long int val = (happiness[i] - (i++));
         if(val >= 0)
+        t += val;
+        k--;
+    }
+    return t;
+}
+*/
+// using max heap
+
+void maxheap(int *h, int n){
+    int i,j,key;
+    for(int k = 1;k<n;k++){
+        i = k;
+        j = (i-1)/2;
+        key = h[i];
+        while(i > 0 && h[j] < key){
+            h[i] = h[j];
+            i = j;
+            j = (i-1)/2;
+        }
+        h[i] = key;
+    }
+}
+
+void adjust(int *h, int *n){
+    int j = 0;
+    int i = 2*j + 1;
+    int key = h[j];
+    while(i < *n){
+        if(i+1 < *n) if(h[i+1] > h[i]) i++;
+        if(h[i] > key){
+            h[j] = h[i];
+            j = i;
+            i = 2*j+1;
+        }
+        else break;
+    }
+    h[j] = key;
+}
+
+int delete(int *h, int *n){
+    int t = h[0];
+    h[0] = h[--(*n)];
+    adjust(h,n);
+    return t;
+}
+
+long long maximumHappinessSum(int* happiness, int happinessSize, int k) {
+    long long int t = 0;
+    maxheap(happiness,happinessSize);
+    int i = 0;
+    while(k && happinessSize){
+        int x = delete(happiness,&happinessSize);
+        int val = x - i++;
+        if(val <= 0) break;
         t += val;
         k--;
     }
